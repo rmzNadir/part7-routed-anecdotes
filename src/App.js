456675eef9c2ea416309c7d837df25db1c14cc46
rmzNadir <dgzrz99@gmail.com>
2021-01-15 21:54:37 -0600

    Exercise 7.2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { Switch, Route, Link, useRouteMatch } from 'react-router-dom';
 
 const Menu = () => {
   const padding = {
@@ -24,12 +24,27 @@ const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
     <ul>
-      {anecdotes.map((anecdote) => (
-        <li key={anecdote.id}>{anecdote.content}</li>
+      {anecdotes.map(({ id, content }) => (
+        <li key={id}>
+          <Link to={`/anecdotes/${id}`}>{content}</Link>
+        </li>
       ))}
     </ul>
   </div>
 );
+
+const Anecdote = ({ anecdote }) => {
+  const { content, votes, info } = anecdote;
+  return (
+    <div>
+      <h2>{content}</h2>
+      <p>has {votes} votes</p>
+      <p>
+        for more info see <a href={info}>{info}</a>
+      </p>
+    </div>
+  );
+};
 
 const About = () => (
   <div>
@@ -154,10 +169,14 @@ const App = () => {
     setAnecdotes(anecdotes.map((a) => (a.id === id ? voted : a)));
   };
 
-  return (
-    //   <About />
+  const match = useRouteMatch('/anecdotes/:id');
 
-    <Router>
+  const anecdote = match
+    ? anecdotes.find((anecdote) => anecdote.id === match.params.id.toString())
+    : null;
+
+  return (
+    <>
       <h1>Software anecdotes</h1>
       <Menu />
       <Switch>
@@ -167,12 +186,15 @@ const App = () => {
         <Route path='/about'>
           <About />
         </Route>
+        <Route path='/anecdotes/:id'>
+          <Anecdote anecdote={anecdote} />
+        </Route>
         <Route path='/'>
           <AnecdoteList anecdotes={anecdotes} />
         </Route>
       </Switch>
       <Footer />
-    </Router>
+    </>
   );
 };
 
